3ac0c5a197864de7eca71df76855d8d8d32f33ad
SanderSchepers1993 <sander.schepers@student.hogent.be>
2025-05-04 22:52:16 +0200

    Update setup_target.sh

--- scripts/setup_target.sh
+++ scripts/setup_target.sh
@@ -1,35 +1,43 @@
 #!/bin/bash
 set -e
 
-echo "[*] Installing Java..."
-sudo apt update
-sudo apt install -y default-jdk wget
+echo "[*] Setting up target environment..."
 
-echo "[*] Preparing vulnerable webapp..."
-cd "$(dirname "$0")/../webapp"
+# Pad-instellingen
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+WEBAPP_DIR="$PROJECT_ROOT/webapp"
+LIB_DIR="$WEBAPP_DIR/libs"
 
-echo "[*] Creating libs/ folder..."
-mkdir -p libs && cd libs
+# 1. Maak libs-map aan
+mkdir -p "$LIB_DIR"
+cd "$LIB_DIR"
 
-# Jetty dependencies
-wget -q https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-server/9.4.44.v20210927/jetty-server-9.4.44.v20210927.jar
-wget -q https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-servlet/9.4.44.v20210927/jetty-servlet-9.4.44.v20210927.jar
-wget -q https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-http/9.4.44.v20210927/jetty-http-9.4.44.v20210927.jar
-wget -q https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-io/9.4.44.v20210927/jetty-io-9.4.44.v20210927.jar
-wget -q https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-util/9.4.44.v20210927/jetty-util-9.4.44.v20210927.jar
-wget -q https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-security/9.4.44.v20210927/jetty-security-9.4.44.v20210927.jar
+# 2. Download Jetty JARs
+echo "[*] Downloading Jetty dependencies..."
+wget -nc https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-server/9.4.44.v20210927/jetty-server-9.4.44.v20210927.jar
+wget -nc https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-servlet/9.4.44.v20210927/jetty-servlet-9.4.44.v20210927.jar
+wget -nc https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-http/9.4.44.v20210927/jetty-http-9.4.44.v20210927.jar
+wget -nc https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-io/9.4.44.v20210927/jetty-io-9.4.44.v20210927.jar
+wget -nc https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-util/9.4.44.v20210927/jetty-util-9.4.44.v20210927.jar
+wget -nc https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-security/9.4.44.v20210927/jetty-security-9.4.44.v20210927.jar
 
-# Log4j
-wget -q https://repo1.maven.org/maven2/org/apache/logging/log4j/log4j-api/2.14.1/log4j-api-2.14.1.jar
-wget -q https://repo1.maven.org/maven2/org/apache/logging/log4j/log4j-core/2.14.1/log4j-core-2.14.1.jar
+# 3. Download Log4j 2.14.1 (kwetsbaar)
+echo "[*] Downloading Log4j 2.14.1..."
+wget -nc https://repo1.maven.org/maven2/org/apache/logging/log4j/log4j-api/2.14.1/log4j-api-2.14.1.jar
+wget -nc https://repo1.maven.org/maven2/org/apache/logging/log4j/log4j-core/2.14.1/log4j-core-2.14.1.jar
 
-# Servlet API
-wget -q https://repo1.maven.org/maven2/javax/servlet/javax.servlet-api/4.0.1/javax.servlet-api-4.0.1.jar
+# 4. Download Servlet API
+wget -nc https://repo1.maven.org/maven2/javax/servlet/javax.servlet-api/4.0.1/javax.servlet-api-4.0.1.jar
 
-cd ..
-
+# 5. Compileer Main.java
 echo "[*] Compiling Main.java..."
+cd "$WEBAPP_DIR"
 javac -cp "libs/*" Main.java
 
-echo "[*] Starting webapp on port 8080..."
-java -cp ".:libs/*" -Dlog4j.configurationFile=log4j2.xml Main
+# 6. Start de webapp
+echo "[*] Starting vulnerable webapp on port 8080..."
+java -cp ".:libs/*" -Dlog4j.configurationFile=log4j2.xml Main &
+WEBAPP_PID=$!
+
+echo "[✓] Webapp gestart (PID: $WEBAPP_PID)"
+echo "    Open in browser: http://localhost:8080"
