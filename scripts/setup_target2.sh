3530a39dfdc7bb94027c2a9bb582050cd32558f6
SanderSchepers1993 <sander.schepers@student.hogent.be>
2025-05-18 07:42:45 +0200

    Create setup_target2.sh

---
+++ scripts/setup_target2.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+echo "[*] Installing dependencies..."
+sudo apt update
+sudo apt install -y wget unzip curl
+
+echo "[*] Downloading AdoptOpenJDK 8u212..."
+cd ~
+wget -q https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u212-b04/OpenJDK8U-jdk_x64_linux_hotspot_8u212b04.tar.gz
+
+echo "[*] Extracting JDK..."
+tar -xzf OpenJDK8U-jdk_x64_linux_hotspot_8u212b04.tar.gz
+sudo mkdir -p /opt/java
+sudo mv jdk8u212-b04 /opt/java/java8u212
+
+echo "[*] Configuring java and javac alternatives..."
+sudo update-alternatives --install /usr/bin/java java /opt/java/java8u212/bin/java 1
+sudo update-alternatives --install /usr/bin/javac javac /opt/java/java8u212/bin/javac 1
+sudo update-alternatives --set java /opt/java/java8u212/bin/java
+sudo update-alternatives --set javac /opt/java/java8u212/bin/javac
+
+echo "[*] Java version set to:"
+java -version
+
+echo "[*] Preparing vulnerable webapp..."
+cd ~/CyberSec25/webapp
+
+if [ ! -f "javax.servlet-api-4.0.1.jar" ]; then
+    echo "[*] Downloading javax.servlet-api..."
+    wget -q https://repo1.maven.org/maven2/javax/servlet/javax.servlet-api/4.0.1/javax.servlet-api-4.0.1.jar
+fi
+
+echo "[*] Compiling Main.java..."
+javac -cp .:javax.servlet-api-4.0.1.jar Main.java
+
+echo "[*] Starting vulnerable webapp on port 8080..."
+java -cp .:javax.servlet-api-4.0.1.jar Main
