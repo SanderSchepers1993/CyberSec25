1a6640c5ce5e9b441ebd73a0403f643cb63f2353
SanderSchepers1993 <sander.schepers@student.hogent.be>
2025-05-18 08:35:47 +0200

    Update setup_target2.sh

--- scripts/setup_target2.sh
+++ scripts/setup_target2.sh
@@ -3,17 +3,20 @@
 set -e
 
 USER_HOME="/home/vboxuser"
+PROJECT_ROOT="$USER_HOME/CyberSec25"
+WEBAPP_DIR="$PROJECT_ROOT/webapp"
+LIB_DIR="$WEBAPP_DIR/libs"
 
 echo "[*] Installing dependencies..."
 sudo apt update
 sudo apt install -y wget unzip curl
 
-echo "[*] Downloading Eclipse Temurin JDK 8u beta from 2021-11-17..."
+echo "[*] Downloading Eclipse Temurin JDK 8u302..."
 cd ~
-wget --show-progress https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u-2021-07-26-19-42-beta/OpenJDK8U-jdk_x64_linux_hotspot_2021-07-26-19-42.tar.gz
+wget -q --show-progress https://github.com/adoptium/temurin8-binaries/releases/download/jdk8u302-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u302b08.tar.gz
 
 echo "[*] Extracting JDK..."
-tar -xzf OpenJDK8U-jdk_x64_linux_hotspot_2021-07-26-19-42.tar.gz
+tar -xzf OpenJDK8U-jdk_x64_linux_hotspot_8u302b08.tar.gz
 sudo mkdir -p /opt/java
 sudo mv jdk8u302-b08 /opt/java/java8ubeta
 
@@ -26,16 +29,39 @@ sudo update-alternatives --set javac /opt/java/java8ubeta/bin/javac
 echo "[*] Java version set to:"
 java -version
 
-echo "[*] Preparing vulnerable webapp..."
-cd "$USER_HOME/CyberSec25/webapp"
+# Maak libs directory
+echo "[*] Creating libs directory..."
+mkdir -p "$LIB_DIR"
+cd "$LIB_DIR"
 
-if [ ! -f "javax.servlet-api-4.0.1.jar" ]; then
-    echo "[*] Downloading javax.servlet-api..."
-    wget -q https://repo1.maven.org/maven2/javax/servlet/javax.servlet-api/4.0.1/javax.servlet-api-4.0.1.jar
-fi
+# Jetty libraries
+echo "[*] Downloading Jetty dependencies..."
+JETTY_VER="9.4.44.v20210927"
+wget -nc https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-server/$JETTY_VER/jetty-server-$JETTY_VER.jar
+wget -nc https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-servlet/$JETTY_VER/jetty-servlet-$JETTY_VER.jar
+wget -nc https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-http/$JETTY_VER/jetty-http-$JETTY_VER.jar
+wget -nc https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-io/$JETTY_VER/jetty-io-$JETTY_VER.jar
+wget -nc https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-util/$JETTY_VER/jetty-util-$JETTY_VER.jar
+wget -nc https://repo1.maven.org/maven2/org/eclipse/jetty/jetty-security/$JETTY_VER/jetty-security-$JETTY_VER.jar
 
+# Log4j
+echo "[*] Downloading Log4j 2.14.1..."
+LOG4J_VER="2.14.1"
+wget -nc https://repo1.maven.org/maven2/org/apache/logging/log4j/log4j-api/$LOG4J_VER/log4j-api-$LOG4J_VER.jar
+wget -nc https://repo1.maven.org/maven2/org/apache/logging/log4j/log4j-core/$LOG4J_VER/log4j-core-$LOG4J_VER.jar
+
+# Servlet API
+echo "[*] Downloading javax.servlet-api..."
+wget -nc https://repo1.maven.org/maven2/javax/servlet/javax.servlet-api/4.0.1/javax.servlet-api-4.0.1.jar
+
+# Compile Main.java
 echo "[*] Compiling Main.java..."
-javac -cp .:javax.servlet-api-4.0.1.jar Main.java
+cd "$WEBAPP_DIR"
+javac -cp "libs/*" Main.java
 
+# Start webapp
 echo "[*] Starting vulnerable webapp on port 8080..."
-java -cp .:javax.servlet-api-4.0.1.jar Main
+java -cp "libs/*:." -Dlog4j.configurationFile=log4j2.xml Main &
+WEBAPP_PID=$!
+echo "[✔] Webapp gestart (PID: $WEBAPP_PID)"
+echo "    Open in browser: http://localhost:8080"
